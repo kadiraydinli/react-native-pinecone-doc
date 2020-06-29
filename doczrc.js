391220cdef21dfbb9b0542a1b1d0fd8a21fa2fbf
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2020-06-29 23:53:09 +0300

    Güncelleme

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,42 @@
+import * as path from "path";
+import FaviconsWebpackPlugin from "favicons-webpack-plugin";
+
+const PUBLIC = path.resolve(__dirname, "public");
+const SRC = path.resolve(__dirname, "src");
+
+/*
+console.log("laaaaaa",PUBLIC);
+
+
+const modifyBundlerConfig = (config) => {
+  /* config.resolve.alias = Object.assign({}, config.resolve.alias, {
+    "@fonts": `${PUBLIC}/fonts`,
+    "@images": `${PUBLIC}/images`,
+    "@components": `${SRC}/theme/components`,
+    "@styles": `${SRC}/theme/styles`,
+  }); */
+
+  /*config.plugins.push(
+    new FaviconsWebpackPlugin({
+      logo: `${PUBLIC}/favicon.ico`,
+      inject: true,
+    })
+  );
+
+  return config;
+};*/
+
 export default {
   title: "React Native Pinecone",
+  description: "Cross Platform React Native UI Toolkit",
+  dest: "./docs",
+  base: "/react-native-pinecone",
+  public: "./public",
+  indexHtml: "public/index.html",
+  htmlContext: {
+    favicon: "/public/images/pinecone.ico",
+  },
+  //modifyBundlerConfig,
   menu: [
     "Getting Started",
     {
@@ -7,4 +44,11 @@ export default {
       menu: ["Avatar", "Badge"],
     },
   ],
+  onCreateWebpackChain: (config) => {
+    config.resolve.alias
+      .set("@fonts", `${PUBLIC}/fonts`)
+      .set("@images", `${PUBLIC}/images`);
+
+    return config;
+  },
 };
--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -3,19 +3,32 @@ import { Helmet } from 'react-helmet-async'
 import iconFont from 'react-native-vector-icons/Fonts/FontAwesome.ttf'
 
 // The doc prop contains some metadata about the page being rendered that you can use.
-const Wrapper = ({ children, doc }) => <React.Fragment>
+const Wrapper = ({ children, doc }) => (
+  <React.Fragment>
     <Helmet>
-        <meta charSet="utf-8" />
-        <style type="text/css">{`@font-face {
+      <meta charSet="utf-8" />
+      <style type="text/css">
+        {`@font-face {
             src: url(${iconFont});
           font-family: FontAwesome;
         }`}
-        </style>
-        <link rel="icon"
-            type="image/png"
-            href="http://example.com/myicon.png"
-        />
+      </style>
+      <link
+        rel="icon"
+        type="image/png"
+        href={"../../images/pinecone.ico"}
+      />
+      {/* <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-153633895-1"
+      ></script>
+      <script>
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'UA-153633895-1');
+      </script> */}
     </Helmet>
     {children}
-</React.Fragment>
+  </React.Fragment>
+);
 export default Wrapper
